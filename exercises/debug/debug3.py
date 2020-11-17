1b8df5af2e9927b34c3225541afe07856aa2ac12
fliem <franz.liem@gmail.com>
2020-11-17 16:58:56 +0100

    ud

--- exercises/debug/debug3.py
+++ exercises/debug/debug3.py
@@ -4,6 +4,6 @@ What would be a more pythonic way to print each name?
 """
 names = ['Simonetta', 'Guy', 'Ueli', 'Alain', 'Ignazio', 'Viola', 'Karin']
 counter = 1
-for name in names:
-    print(name[counter])
+while counter <= 7:
+    print(names[counter])
     counter = counter + 1
